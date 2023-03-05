a02c705b9f33eb18e74f828f855765f74734ff13
SergioK12 <sergioabd460@gmail.com>
2023-03-05 04:36:28 +0100

    Agregada la funcionalidad get hacia firebase

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,33 +1,34 @@
 import 'dart:convert';
 
 class Producto {
-    Producto({
-        required this.disponibilidad,
-         this.imagen,
-        required this.nombre,
-        required this.precio,
-    });
+  Producto({
+    required this.disponibilidad,
+    this.imagen,
+    required this.nombre,
+    required this.precio,
+  });
 
-    bool disponibilidad;
-    String? imagen;
-    String nombre;
-    int precio;
+  bool disponibilidad;
+  String? imagen;
+  String nombre;
+  int precio;
+  String? id;
 
-    factory Producto.fromJson(String str) => Producto.fromMap(json.decode(str));
+  factory Producto.fromJson(String str) => Producto.fromMap(json.decode(str));
 
-    String toJson() => json.encode(toMap());
+  String toJson() => json.encode(toMap());
 
-    factory Producto.fromMap(Map<String, dynamic> json) => Producto(
+  factory Producto.fromMap(Map<String, dynamic> json) => Producto(
         disponibilidad: json["Disponibilidad"],
         imagen: json["Imagen"],
         nombre: json["Nombre"],
         precio: json["Precio"],
-    );
+      );
 
-    Map<String, dynamic> toMap() => {
+  Map<String, dynamic> toMap() => {
         "Disponibilidad": disponibilidad,
         "Imagen": imagen,
         "Nombre": nombre,
         "Precio": precio,
-    };
+      };
 }
--- lib/views/homeview.dart
+++ lib/views/homeview.dart
@@ -1,33 +1,29 @@
+import 'package:crud_servicos/services/product_service.dart';
 import 'package:crud_servicos/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final productprovider = Provider.of<ProductsServices>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Productos'),
       ),
       body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (BuildContext context, index )=> Padding(
-          padding:  EdgeInsets.symmetric(horizontal: (MediaQuery.of(context).size.height * 0.02)),
-          child: GestureDetector(
-            child: const ProductCard(),
-            onTap: ()=> Navigator.pushNamed(context, 'product'),),
-            
-        )
-
-
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: (){
-
-        }
-        ),
-      
+          itemCount: 10,
+          itemBuilder: (BuildContext context, index) => Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: (MediaQuery.of(context).size.height * 0.02)),
+                child: GestureDetector(
+                  child: const ProductCard(),
+                  onTap: () => Navigator.pushNamed(context, 'product'),
+                ),
+              )),
+      floatingActionButton:
+          FloatingActionButton(child: const Icon(Icons.add), onPressed: () {}),
     );
   }
 }
